d2a6a008cc90be30baeaf0056d57e89f909138da
ShashikaUdara <techdudelab@gmail.com>
2022-06-01 00:03:22 +0530

    forms

---
+++ PHP Form/welcome.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <?php
+            $user_name = $_POST["name"];
+            $user_email = $_POST["email"];
+            echo $user_name . " - " . $user_email;
+        ?>
+    </body>
+</html>
