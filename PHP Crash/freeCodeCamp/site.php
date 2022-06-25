ebc7a21ead7102c51972f12a04d50010d4e7eb88
ShashikaUdara <techdudelab@gmail.com>
2022-06-25 23:26:15 +0530

    git crash cource started

---
+++ PHP Crash/freeCodeCamp/site.php
@@ -0,0 +1,13 @@
+<!-- PracticePHP/PHP Crash/freeCodeCamp/site.php -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo("Hello World");
+     ?>
+  </body>
+</html>
